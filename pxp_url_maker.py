59832bd1d370a887f8f667a37fe50b0e54138c7d
Yoshi <KingXiaoDi@users.noreply.github.com>
2017-02-20 06:40:41 -0500

    Add files via upload

--- pxp_url_maker.py
+++ pxp_url_maker.py
@@ -1,50 +1,46 @@
-from team_info.name_converter import get_abr
 from sqlalchemy import create_engine
 from bs4 import BeautifulSoup
 import pandas as pd
 import datetime
-import numpy
 
-def home_team(winner, loser, location):
-	home = [];
-	for i in range(0,16):
+def get_abr(name):
+	df = pd.read_csv("c:/users/yoshi/Documents/Python_Scripts/nfl/pxp/team_info/team_info.csv")
+	return df.loc[df['Team'] == name]['Abr'].values
+	a = df.loc[df['Team'] == name]['Abr'].values
+	
+def home_team(winner, loser, location, games):
+	home = []
+	for i in range(0,games):
 		if location[i] == "@":
-			home.append(loser[i])#.lower());
+			home.append(loser[i])#.lower())
 		else:
-			home.append(winner[i])#.lower());
-	abr = [];
+			home.append(winner[i])#.lower())
+	abr = []
 	for teams in home:
-		abr.append(get_abr(teams));
-	return (abr);
+		abr.append(get_abr(teams))
+	return (abr)
 	
 def date_formatter(year, date):
 	formatted = []
 	for dates in date:
-		new = dates + " " + str(year);
-		test = str(datetime.datetime.strptime(new, '%B %d %Y').date());
-		test = test + "0";
-		test = test.replace("-","");
-		formatted.append(test);
-	return formatted;
+		new = dates + " " + str(year)
+		test = datetime.datetime.strptime(new, '%B %d %Y').date()
+		if test.month == 1 or test.month == 2:
+			new = dates + " " + str(year+1)
+			test = datetime.datetime.strptime(new, '%B %d %Y').date()
+		test = str(test) + "0"
+		test = test.replace("-","")
+		formatted.append(test)
+	return formatted
 	
 def yearly_sched(year, team):
-	con = create_engine("mysql+pymysql://root:@127.0.0.1/nfl"); 
-	sched = pd.read_sql("select `Date`, `Winner`, `Location`, `Loser` from `" + str(year)+"` where `Winner` = \""+team+"\" or `Loser` = \""+team+"\"", con);
-	home = home_team(sched["Winner"], sched["Loser"], sched["Location"]);
-	dates = date_formatter(year, sched["Date"]);
-	url = [];
-	for i in range(0,16):
-		url.append(dates[i]+home[i]);
-		url[i] = (url[i])[0]
-	return url;
-	#return (sched["Date"], home);
-	#print (sched["Date"]);
-	#print (sched["Winner"]);
-	#print (sched["Location"]);
-	#print (sched["Loser"]);
-	
-
-add = yearly_sched(2016, "Baltimore Ravens");
-for i in add:
-	site = "http://www.pro-football-reference.com/boxscores/"+i+".htm";
-	print (site);
+	con = create_engine("mysql+pymysql://root:@127.0.0.1/nfl") 
+	sched = pd.read_sql("select `Date`, `Winner`, `Location`, `Loser` from `" + str(year)+"` where `Winner` = \""+team+"\" or `Loser` = \""+team+"\"", con)
+	games = len(sched.index)
+	home = home_team(sched["Winner"], sched["Loser"], sched["Location"], games)
+	dates = date_formatter(year, sched["Date"])
+	url_ending = []
+	for i in range(0,games):
+		url_ending.append(dates[i]+home[i])
+		url_ending[i] = (url_ending[i])[0]
+	return url_ending
--- pxpsorter.py
+++ pxpsorter.py
@@ -1,28 +1,52 @@
-import requests
 import pandas as pd
-from bs4 import Comment, BeautifulSoup
- 
-r = requests.get("http://www.pro-football-reference.com/boxscores/201612250pit.htm")
- 
-b = BeautifulSoup(r.text, "lxml")
- 
- 
-l = b.find("div", { "id" : "all_pbp" } )
-comments = l.findAll(text=lambda text:isinstance(text, Comment))
- 
-commentsoup = BeautifulSoup(comments[0], "lxml")
+import math
 
-df = pd.read_html(str(commentsoup))[0];
-df = df[["Quarter", "Time", "Down", "ToGo", "Location", "Detail", "BAL", "PIT"]];
+def sorter(file):
+	df = pd.read_csv(file)
+	td = (df[df["Play"].astype(str).str.contains('touchdown')])
+	pat = (df[df["Play"].astype(str).str.contains('extra point')])
+	fg = (df[df["Play"].astype(str).str.contains('field goal')])
+	ko = (df[df["Play"].astype(str).str.contains('kicks off')])
+	onside = (df[df["Play"].astype(str).str.contains('kicks onside')])
+	punt = (df[df["Play"].astype(str).str.contains('punt')])
+	sack = (df[df["Play"].astype(str).str.contains('sack')])
+	penalty = (df[df["Play"].astype(str).str.contains('Penalty')])
+	timeout = (df[df["Play"].astype(str).str.contains('Timeout')])
+	clock = (df[df["Play"].astype(str).str.contains('spike')])
+	throws = (df[df["Play"].astype(str).str.contains('pass')])
+	runs = (df[df["Play"].astype(str).str.contains('Penalty')==False])
+	runs = (runs[runs["Play"].astype(str).str.contains('pass')==False])
+	runs = (runs[runs["Play"].astype(str).str.contains('kicks off')==False])
+	runs = (runs[runs["Play"].astype(str).str.contains('sack')==False])
+	runs = (runs[runs["Play"].astype(str).str.contains('punt')==False])
+	runs = (runs[runs["Play"].astype(str).str.contains('extra point')==False])
+	runs = (runs[runs["Play"].astype(str).str.contains('Timeout')==False])
+	runs = (runs[runs["Play"].astype(str).str.contains('field goal')==False])
+	runs = (runs[runs["Play"].astype(str).str.contains('spike')==False])
+	number = len(pat.index)+len(fg.index)+len(ko.index)+len(onside.index)+len(punt.index)+len(sack.index)+len(penalty.index)+len(timeout.index)+len(clock.index)+len(throws.index)
+	print (len(df.index), number , len(runs.index))
+	col = ["Quarter", "Time Rem.", "Down", "Yards", "Location", "Poss", "Gun", "Form", "Play", "Away", "Home"]
+	print (td[col])
+#sorter("c:/users/yoshi/documents/python_scripts/nfl/pxp/games/201612040rav.csv")
 
-#df.to_csv("c:/users/yoshi/documents/python scripts/pbp.csv");
-
-throw = (df[df["Detail"].astype(str).str.contains('pass')]);
-ko = (df[df["Detail"].astype(str).str.contains('kicks off')]);
-onside = (df[df["Detail"].astype(str).str.contains('kicks onside')]);
-punt = (df[df["Detail"].astype(str).str.contains('punt')]);
-sack = (df[df["Detail"].astype(str).str.contains('sack')]);
-penalty = (df[df["Detail"].astype(str).str.contains('Penalty')]);
-other = (df[df["Detail"].astype(str).str.contains('Penalty')==False]);
-other = (other[other["Detail"].astype(str).str.contains('pass')==False]);
-print (punt);
+def yard_adj(file):
+	df = pd.read_csv(file)
+	pos = df["Poss"]
+	yard = df["Location"]
+	yard = yard.fillna(method='ffill')
+	for i in range(0, (len(pos.index))):
+		pos[i] = pos[i].upper()
+		if yard[i] == " 50":
+			yard[i] = 50
+		else:
+			a = yard[i].split()[0]
+			b = yard[i].split()[1]
+		if pos[i] == "KO":
+			yard[i] = b
+		elif a == pos[i]:
+			yard[i] = b
+		else:
+			yard[i] = 100- int(b)
+	df["Location"] = yard
+	df.to_csv(file)
+yard_adj("c:/users/yoshi/documents/python_scripts/nfl/pxp/games/201612040rav.csv")
